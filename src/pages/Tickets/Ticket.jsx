a14fd0f7dafa04810d9c47d41287ac46c419cb2c
Ashitosh <ashitosh.k@raasoft.com>
2024-08-15 19:27:59 +0530

    .

--- src/pages/Tickets/Ticket.jsx
+++ src/pages/Tickets/Ticket.jsx
@@ -68,7 +68,7 @@ const Ticket = () => {
               </strong>
               <br />
               <a
-                href='https://www.palacetheaterct.org/'
+                href='https://www.palacetheaterct.org/shows-and-events/main-stage/hocus-pocus-live?utm_content=bufferd5bf2&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer&fbclid=IwY2xjawEnNe9leHRuA2FlbQIxMAABHRbkt8iVXlCoHvpKHjywoG61z59ALwQIf9I00EBONf6Bj6RSrCL-qA_7yg_aem_R1liLiM6Vmiu6F_1qNBk0g'
                 target='_blank'
                 rel='noopener noreferrer'
               >
@@ -83,6 +83,35 @@ const Ticket = () => {
               <br />
               <a
                 href='https://gardearts.org/events/hocus-pocus/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Click for tickets
+              </a>
+            </p>
+            <p>
+              <strong>
+                The Salem Express: Revenge of the Sanderson Sisters train ride
+                experience- September 20 &21 4:45 pm & 7:15 pm (Check in Times)
+                each day.
+              </strong>
+              <br />
+              <a
+                href='https://essexsteamtrain.com/experiences-2/the-salem-express-revenge-of-the-sanderson-sisters/?fbclid=IwY2xjawEnMNtleHRuA2FlbQIxMAABHXQjpPY2E2kfhOXRKV-H3VNT13-e2kJC2pezvYI0ffXs1YREU6VYv6xWsw_aem_AXsXHmI6FGL6GeM9KC8g9Q'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Click for tickets
+              </a>
+            </p>{' '}
+            <p>
+              <strong>
+                "Just a Brunch of Hocus Pocus"- Oct 20 Seating 11am showtime
+                12pm.
+              </strong>
+              <br />
+              <a
+                href='https://essexsteamtrain.com/experiences-2/just-a-brunch-of-hocus-pocus/?fbclid=IwY2xjawEp6rRleHRuA2FlbQIxMAABHb1FpJA2NTs9PrrkeS8iSt3gsT0pkz-kj6HiLXszNYT9_rkiWqkfFb5sNA_aem_EdnChd9KNVQyWqAvYIQ9yQ'
                 target='_blank'
                 rel='noopener noreferrer'
               >
